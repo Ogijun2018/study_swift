3ff6bc69cef380e85621dac0d612827eb070c958
Ogijun2018 <ogijun.mk@hotmail.co.jp>
2020-05-30 00:21:46 +0900

    一時間目終わり

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -30,5 +30,59 @@ var str: String = "String" //文字列はString, 文字はCharacter
 var power = true
 power = false
 
-//タプル
+//データ型を指定しないタプル
+var item = ("iPod", "iPhone", "iPad")
+/*
+ これでも可
+ var item = (IPOD: "iPod", IPHONE: "iPhone", IPAD: "iPad")
+ */
+var person = ("Steve Jobs", 56)
 
+//データ型を指定するタプル
+var item2:(String, String, String) = ("iPod", "iPhone", "iPad")
+var person2:(String, Int) = ("Steve Jobs", 56)
+
+print(item.0, item.1, item.2)
+
+//複数の定数をタプルデータで初期化する
+let(IPOD, IPHONE, IPAD) = item
+print(IPOD, IPHONE, IPAD)
+
+//配列…タプルと違い同じデータ型しか入れることができない
+//let PRIME_NUMBER = [2,3,5,7,11]
+//let fruit = ["Apple", "Banana", "Orange"]
+
+let PRIME_NUMBER: [Int] = [2,3,5,7,11]
+let fruit: [String] = ["apple", "banana", "orange"]
+
+//配列の最初と最後の要素を表示する
+//データの中身が空になる可能性がある値は、オプショナルバリューという特別な値になる
+print(PRIME_NUMBER.first, PRIME_NUMBER.last)
+
+//配列の要素の追加、挿入、削除
+var fruit2: [String] = ["Apple", "Banana", "Orange"]
+fruit2.append("Kiwi")
+fruit2.remove(at: 0)
+fruit2.insert("Strawberry", at: 1)
+print(fruit2)
+
+//空の配列の作成
+var Fruit: [String] = []
+Fruit.append("Apple")
+Fruit.append("Banana")
+
+//配列の要素をすべて同じ値で初期化する
+var Fruit2 = [String](repeating: "Apple", count: 5)
+
+//辞書型
+let Fruit3 = ["Apple": "りんご", "Banana": "ばなな", "Orange": "オレンジ"]
+var award = [1: "アカデミー賞", 2: "ノーベル平和賞", 3: "芥川賞"]
+print(Fruit3["Apple"])
+print(award[1])
+
+
+//確認テスト
+var Numbers: Int = 5
+var osName:(String, String) = ("OS X", "iOS")
+var language:[String] = ["Swift", "Objective-C", "Java"]
+let vegetables = [1:"tomato", 2:"potato", 3:"carrot"]
