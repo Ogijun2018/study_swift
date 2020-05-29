806f83c3a985dcc3d6151d11f97ad65e5e726a28
Ogijun2018 <ogijun.mk@hotmail.co.jp>
2020-05-29 23:50:51 +0900

    studying Swift

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+var luckyNo: Int = 7
+luckyNo = 3
+var badNo = 5
+//badNo = "Sample"
+
+let SQRT2: Double = 1.141421356
+let TAX = 1.08
+
+print(SQRT2)
+print(TAX)
+
+var name: String = "String" //文字列
+var age: Int = 6 //整数
+var weight: Double = 58.5 //浮動小数点
+
+var bin = 0b0111 //2進数を代入
+var oct = 0o12 //8進数を代入
+var hex = 0x1A //16進数を代入
+
+print(bin)
+print(oct)
+print(hex) //10進数で表示される
+
+var kugiri: Int = 10_000 //アンダースコアで区切っても表示は通常通り
+print(kugiri)
+
+var chr: Character = "c"
+var str: String = "String" //文字列はString, 文字はCharacter
+
+var power = true
+power = false
+
+//タプル
+
